b899f7f5732297ed5f620bd624d8346b240c5e90
xm343 <abhijithsivadas01@gmail.com>
2026-04-27 11:53:25 +0530

    tree constructor

---
+++ trees/constructor.js
@@ -0,0 +1,13 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class bst{
+    constructor(){
+        this.root = null
+    } 
+}
