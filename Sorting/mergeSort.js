3a090d4903e8c036ffaae51d9c41841a0f0a4248
xm343 <abhijithsivadas01@gmail.com>
2026-05-13 07:35:21 +0530

    mergesort

---
+++ Sorting/mergeSort.js
@@ -0,0 +1,27 @@
+function mergeSort(array1, array2){
+    let combined = []
+    let i=0;
+    let j=0;
+    while(i<array1.length && j<array2.length){
+        if(array1[i]<array2[j]){
+            combined.push(array1[i])
+            i++
+        }
+        else{
+            combined.push(array2[j])
+            j++
+        }
+    }
+    while(i<array1.length){
+        combined.push(array1[i])
+        i++
+    }
+    while(j<array2.length){
+        combined.push(array2[j])
+        j++
+    }
+    return combined
+}
+let arr1 = [1,2,3,7,8]
+let arr2 = [4,5,6,9,10]
+console.log(mergeSort(arr1,arr2))
